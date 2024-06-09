94e5ebb8251a4e7cf1620afde3af08893c7f92d9
Dmytro <dmytro.kupets@gmail.com>
2024-06-09 15:03:20 +0300

    fix: bugs with routes

--- src/router/exchangeRate.js
+++ src/router/exchangeRate.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const exchangeRateController = require('../controller/exchangeRate');
 
 router.get('/exchangeRates', exchangeRateController.getExchangeRates);
+router.get('/exchangeRate/:currencyPair', exchangeRateController.getExchangeRatesByCodes);
 router.post('/exchangeRates', exchangeRateController.postExchangeRates);
 router.patch('/exchangeRate/:currencyPair', exchangeRateController.updateExchangeRate);
 
